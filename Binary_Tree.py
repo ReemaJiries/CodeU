a1a71eb84dcc936f96b6c7d2c2f2c73a00756555
ReemaJiries <reema.jiriees@gmail.com>
2017-05-29 00:36:42 +0300

    Update Binary_Tree.py
    Adding documentation of the class functions
    and update the add function to include adding binary tree

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -1,4 +1,9 @@
 from Node import Node
+
+"""
+the Binary Tree consist of Nodes with no duplicates keys and root node.
+onstructor: accepts key of the root Node
+"""
 class Binary_Tree():
     def __init__(self, root_key = 0):
         if root_key == 0:
@@ -7,7 +12,16 @@ class Binary_Tree():
         else:
             self._root = Node(root_key)
             self._size = 1
+    """
+    returns the root node of binary tree
+    """
+    def get_root(self):
+        return self._root
 
+    """
+    Input:integer key in the array and optional start search node (the default is root Node)
+    Output:the node with the given key
+    """
     def search(self,key, search_node = None):
         if search_node is None:
             search_node = self._root
@@ -19,28 +33,52 @@ class Binary_Tree():
             return self.search(key, search_node.getRightChild())
         if search_node.getLeftChild():
             return self.search(key, search_node.getLeftChild())
+    """
+    Input: key of Integer type or Binary Tree, parent node key, boolean is_right
+    is right child or left.
+    Output: True or False.
+    """
     def add(self,key, parent_key = None, is_right = True):
         if self._size == 0:
             self._root = Node(key)
         else:
-            if self.search(key):
-                print("Error: the given key {0} is exist in the Binar Tree".format(key))
+            if type(key) is Binary_Tree:
+                if  self.search(key.get_root().getData()):
+                    print("Error: the given key {0} is exist in the Binar Tree".format(key.get_root().getData()))
+                    return False
+            elif isinstance(key,int):
+                if self.search(key):
+                    print("Error: the given key {0} is exist in the Binar Tree".format(key))
+                    return False
+            else:
+                print("Error: the type of key isn't integer or Binary_Tree")
                 return False
             temp_parent = self.search(parent_key, self._root)
-            temp = Node(key, temp_parent)
+            if isinstance(key,int):
+                temp = Node(key, temp_parent)
+            else:
+                temp = key.get_root()
             if not is_right:
                 if temp_parent.getLeftChild():
                     print("Error: the parent with the given key {0} had left child".format(key))
                     return False
+                temp.setParent(temp_parent)
                 temp_parent.setLeftChild(temp)
             else:
                 if temp_parent.getRightChild():
                     print("Error: the parent with the given key {0} had right child".format(key))
                     return False
+                temp.setParent(temp_parent)
                 temp_parent.setRightChild(temp)
         self._size += 1
         return True
-        
+    
+    """
+    Given a Binary Tree and a key, prints all the ancestors of the key in
+    the given binary tree.
+    Input: key of type Integer
+    Output: list of all the ancestors of the key in the given binary tree
+    """
     def getAncestors(self, key):
         key_node = self.search(key)
         ancestors = []
@@ -52,8 +90,13 @@ class Binary_Tree():
         while key_node.getParent() is not None:
             ancestors.append(key_node.getParent().getData())
             key_node = key_node.getParent()
+        print(ancestors)
         return ancestors
-
+    
+    """
+    Input: 2 Keys of type Integer
+    Output: return he lowest common ancestor of two nodes in a binary tree
+    """
     def commonAncestor(self, key1, key2):
         if self.search(key1) is None or self.search(key2) is None:
             print("Error: one of the given keys isn't exist in the BT")
@@ -80,4 +123,3 @@ class Binary_Tree():
                 for an in range(size):
                     if secondeAncestors[an] in firstAncestors:
                         return secondeAncestors[an]
-
